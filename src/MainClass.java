c72a38d3d2681d29e7ab05d5dbc6eec73708d049
Jay2cof <justice101onuoha@gmail.com>
2024-03-15 11:13:10 +0100

    Working in SQL FetchingDatafromSQLdatabase.java

---
+++ src/MainClass.java
@@ -0,0 +1,6 @@
+public class MainClass {
+    public static void main(String[] args) {
+        Runtime.Version version = Runtime.version();
+        System.out.println("Java version: " + version);
+    }
+}
